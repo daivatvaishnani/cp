1eb1c0a1c080ecc5e1dd91f74fceae308aa870bb
Daivat Vaishnani <daivatvaishnani18@gmail.com>
2020-12-27 16:21:55 +0530

    some more codes

--- codes/L56Laby.cpp
+++ codes/L56Laby.cpp
@@ -4,57 +4,61 @@ using namespace std;
 
 const int maxnm = 1005; 
 
-int effects[maxnm][maxnm], pathcost[maxnm][maxnm];
+int arr[maxnm][maxnm], arrv[maxnm][maxnm];
+bool poss[maxnm][maxnm];
 
-bool ok(int x, int y, int n, int m) {
+bool valid(int x, int y, int n, int m) {
 	if(x <= n && x >= 1 && y <= m && y >= 1) return true;
 	else return false;
 }
 
 int main() {
-	int T;
-	cin >> T;
-	while(T--) {
-		int N, M;
-		memset(effects, 0, sizeof(effects));
-		memset(pathcost, 0, sizeof(pathcost));
-		cin >> N >> M;
-		for (int i = 1; i <= N; ++i) {
-			for (int j = 1; j <= M; ++j) {
-				cin >> effects[i][j];
+	// freopen("in", "r", stdin);
+	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int t;
+	cin >> t;
+	while(t--) {
+		int n, m;
+		memset(arr, 0, sizeof(arr));
+		memset(arrv, 0, sizeof(arrv));
+		memset(poss, 0, sizeof(poss));
+		cin >> n >> m;
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				cin >> arr[i][j];
 			}
 		}
-		for(int i = 1; i <= N; ++i) {
-			for (int j = 1; j <= M; ++j) {
-				if(effects[i][j] > 0) ++effects[i][j];
+		for(int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				if(arr[i][j] > 0) ++arr[i][j];
 			}
 		}
 		int cnt = 100;
 		while(cnt--) {	
-			for(int i = 1; i <= N; ++i) {
-				for (int j = 1; j <= M; ++j) {
-					pathcost[i][j] = effects[i][j];
-					if(effects[i][j] != -1) {
-						if(ok(i, j + 1, N, M)) {
-							pathcost[i][j] = max(pathcost[i][j], pathcost[i][j + 1] - 1);
+			for(int i = 1; i <= n; ++i) {
+				for (int j = 1; j <= m; ++j) {
+					arrv[i][j] = arr[i][j];
+					if(arr[i][j] != -1) {
+						if(valid(i, j + 1, n, m)) {
+							arrv[i][j] = max(arrv[i][j], arrv[i][j + 1] - 1);
 						}
-						if(ok(i + 1, j, N, M)) {
-							pathcost[i][j] = max(pathcost[i][j], pathcost[i + 1][j] - 1);
+						if(valid(i + 1, j, n, m)) {
+							arrv[i][j] = max(arrv[i][j], arrv[i + 1][j] - 1);
 						}
-						if(ok(i, j - 1, N, M)) {
-							pathcost[i][j] = max(pathcost[i][j], pathcost[i][j - 1] - 1);
+						if(valid(i, j - 1, n, m)) {
+							arrv[i][j] = max(arrv[i][j], arrv[i][j - 1] - 1);
 						}
-						if(ok(i - 1, j, N, M)) {
-							pathcost[i][j] = max(pathcost[i][j], pathcost[i - 1][j] - 1);
+						if(valid(i - 1, j, n, m)) {
+							arrv[i][j] = max(arrv[i][j], arrv[i - 1][j] - 1);
 						}
 					}
 				}
 			}
 		}
-		for (int i = 1; i <= N; ++i) {
-			for (int j = 1; j <= M; ++j) {
-				if(pathcost[i][j] == -1) cout << "B";
-				else if(pathcost[i][j] > 0) cout << "Y";
+		for (int i = 1; i <= n; ++i) {
+			for (int j = 1; j <= m; ++j) {
+				if(arrv[i][j] == -1) cout << "B";
+				else if(arrv[i][j] > 0) cout << "Y";
 				else cout << "N";
 			}
 			cout << "\n";
--- codes/l56kth.cpp
+++ codes/l56kth.cpp
@@ -2,50 +2,58 @@
 
 using namespace std;
 
-long long N;
-const long long MAXN = 50001;
-const long long INF = 1e9;
-vector<long long> V;
-long long A[MAXN], PRE[MAXN], T[2*MAXN];
+long long n;
+const long long maxn = 50001;
+const long long inf = 1e9;
+vector<long long> v;
+long long arr[maxn], prexor[maxn], t[2*maxn];
 
-long long MINin(long long l, long long r) {
-	long long res = INF;
-	for (l += N, r += N; l < r; l >>= 1, r >>= 1) {
-		if (l&1) res = min(res, T[l++]);
-		if (r&1) res = min(res, T[--r]);
+long long query(long long l, long long r) {
+	long long res = inf;
+	for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+		if (l&1) res = min(res, t[l++]);
+		if (r&1) res = min(res, t[--r]);
 	}
 	return res;
 }
 
+void build(long long n) {
+	for (int i = n - 1; i >= 1; --i) t[i] = min(t[i<<1], t[i<<1|1]);
+}
+
 int main() {
 	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-	cin >> N;
+	// freopen("in", "r", stdin);
+	cin >> n;
 	int k;
 	cin >> k;
-	for (int i = 0; i < N; ++i) {
-		cin >> A[i];
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
 	}
-	PRE[0] = A[0];
-	for(int i = 1; i < N; ++i) {
-		PRE[i] = PRE[i - 1] ^ A[i];
+	prexor[0] = arr[0];
+	for(int i = 1; i < n; ++i) {
+		prexor[i] = prexor[i - 1] ^ arr[i];
 	}
-	for(int i = 0; i < N; ++i) {
-		T[i + N] = A[i];
+	for(int i = 0; i < n; ++i) {
+		t[i + n] = arr[i];
 	}
-	for (int i = N - 1; i >= 1; --i) T[i] = min(T[i<<1], T[i<<1|1]);
-	for(int l = 0; l < N; ++l) {
-		for(int r = l; r < N; ++r) {
-			long long MIN = MINin(l, r + 1), XOR;
+	build(n);
+	int sum = 0;
+	for(int l = 0; l < n; ++l) {
+		for(int r = l; r < n; ++r) {
+			++sum;
+			long long val, minm = query(l, r + 1), xorr;
 			if(l == 0) {
-				XOR = PRE[r];
+				xorr = prexor[r];
 			}
 			else {
-				XOR = PRE[r]^PRE[l - 1];
+				xorr = prexor[r]^prexor[l - 1];
 			}
-			sort(V.begin(), V.end());
-			V.push_back(MIN * XOR);
+			val = minm * xorr;
+			v.push_back(val);
 		}
 	}
-	cout << V[k - 1] << "\n";
+	sort(v.begin(), v.end());
+	cout << v[k - 1] << "\n";
 	return 0;
 }
